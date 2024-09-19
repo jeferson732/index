fb02c69a2ca9c0811c55005947893b7bf9ecb938
jeferson732 <jefmaz29@gmail.com>
2024-09-18 19:18:30 -0500

    mi repositorio corregido

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express ();
+const path = require('path');
+const PORT = 3000;
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.send('MI PORTAFOLIO');
+});
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
